fc5afb777d6463f4b8146a9482fd5b97d3589fdd
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2017-08-06 14:40:56 +0100

    Updated naming for methods

--- FluentString/FluentStringExtensions.cs
+++ FluentString/FluentStringExtensions.cs
@@ -8,14 +8,14 @@ namespace System
 {
     public static class FluentStringExtensions
     {
-        public static bool IsNullOrWhiteSpace(this string text)
+        public static bool IsNullOrWhiteSpace(this string value)
         {
-            return String.IsNullOrWhiteSpace(text);
+            return String.IsNullOrWhiteSpace(value);
         }
 
-        public static bool IsNullOrWhiteSpaceOrDefault(this string text, string defaultString)
+        public static bool IsNullOrWhiteSpaceOrDefault(this string value, string defaultValue)
         {
-            return String.IsNullOrWhiteSpace(text) || text == defaultString;
+            return String.IsNullOrWhiteSpace(value) || value == defaultValue;
         }
     }
 }
