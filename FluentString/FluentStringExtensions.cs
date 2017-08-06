9fddfbc8d6ec2d40e2678136daf113a71f91f84b
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2017-08-06 13:57:07 +0100

    String methods added so that tests compile

---
+++ FluentString/FluentStringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class FluentStringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsNullOrWhiteSpaceOrDefault(this string text, string defaultString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
